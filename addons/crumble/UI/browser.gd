217f5e8da045ab6d0c53f4554bafd9d0928d96d4
Pay <101824005+Paolo-Antonini@users.noreply.github.com>
2026-05-12 01:07:49 +0200

    feat: add item lsit

--- addons/crumble/UI/browser.gd
+++ addons/crumble/UI/browser.gd
@@ -1,13 +1,22 @@
 @tool
 extends VBoxContainer
 
+var dictionnaire = {
+	"PSX": ["Shader1", "Shader2"],
+	"Horror": ["Shader3"],
+	"Sci-Fi": ["Shader4"]
+}
 func _ready() -> void:
 	$OptionButton.add_item("PSX")
-	$OptionButton.add_separator("")
 	$OptionButton.add_item("Horror")
-	$OptionButton.add_separator("")
 	$OptionButton.add_item("Sci-Fi")
+	$OptionButton.item_selected.connect(update_list)
+	$ItemList.size_flags_vertical = Control.SIZE_EXPAND_FILL
+	update_list(0)
 
-
-func _process(delta: float) -> void:
-	pass
+func update_list(index: int) -> void:
+	$ItemList.clear()
+	var categorie = $OptionButton.get_item_text(index)
+	for shader in dictionnaire[categorie] :
+		$ItemList.add_item(shader)
+		

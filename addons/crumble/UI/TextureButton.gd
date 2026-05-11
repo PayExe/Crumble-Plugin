06db3914cbb98f0dde03637eb785d2af6517af36
Pay <101824005+Paolo-Antonini@users.noreply.github.com>
2026-05-12 00:16:24 +0200

    feat: open a windows

--- addons/crumble/UI/TextureButton.gd
+++ addons/crumble/UI/TextureButton.gd
@@ -1,9 +1,14 @@
 @tool
 extends TextureButton
 
+var popup
+
 func _ready() -> void:
-	pass
+	popup = Window.new()
+	popup.title = "Crumble"
+	get_tree().root.add_child(popup)
+	pressed.connect(open)
 
-
-func _process(delta: float) -> void:
-	pass
+func open() -> void:
+	popup.size = Vector2i(600, 500)
+	popup.popup_centered()
